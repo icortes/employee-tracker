ad31b554e68590ee4a689afcda86100051f6967e
isa <5445946+icortes@users.noreply.github.com>
2021-10-22 21:39:51 -0700

    added code to connect to database

--- server.js
+++ server.js
@@ -2,55 +2,77 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
 
+// Connect to database
+const db = mysql.createConnection({
+        host: 'localhost',
+        // MySQL username
+        user: 'root',
+        // MySQL password
+        password: 'password',
+        database: 'company_db'
+    },
+    console.log(`Connected to the company_db database.`)
+);
+
 function init() {
     inquirer.prompt([{
-        type: "list",
-        name: "option",
-        message: "What would you like to do?",
-        choices: [
-            "view all departments",
-            "view all roles",
-            "view all employees",
-            "add a department",
-            "add a role",
-            "add an employee",
-            "update an employee role"
-        ]
-    }])
-    .then(response => {
+            type: "list",
+            name: "option",
+            message: "What would you like to do?",
+            choices: [
+                "view all departments",
+                "view all roles",
+                "view all employees",
+                "add a department",
+                "add a role",
+                "add an employee",
+                "update an employee role",
+                "QUIT"
+            ]
+        }])
+        .then(response => {
 
-        const option = response.option;
-        //console.log(option);
-        switch(option) {
-            case "view all departments":
-                //do some work
-                break;
-            case "view all roles":
-                //do some work
-                break;
-            case "view all employees":
-                //do some work
-                break;
-            case "add a department":
-                // do some work
-                break;
-            case "add a role":
-                //do some work
-                break;
-            case "add an employee":
-                //dp some work
-                break;
-            case "update an employee role":
-                //so some work
-            default:
-                console.log("this is not an option");
-                break;
+            const option = response.option;
+            //console.log(option);
+            switch (option) {
+                case "view all departments":
+                    //do some work
+                    break;
+                case "view all roles":
+                    //do some work
+                    break;
+                case "view all employees":
+                    //do some work
+                    break;
+                case "add a department":
+                    // do some work
+                    break;
+                case "add a role":
+                    //do some work
+                    break;
+                case "add an employee":
+                    //do some work
+                    break;
+                case "update an employee role":
+                    //do some work
+                    break;
+                case "QUIT":
+                    //stop the program
+                    break;
+                default:
+                    console.log("this is not an option");
+                    break;
 
-        }
-    })
-    .catch((error) => {
-        error ? console.log(error) : null;
-    });
+            }
+        })
+        .catch((error) => {
+            error ? console.log(error) : null;
+        });
+}
+
+
+function viewAllDepartments() {
+
 }
 
 init();
